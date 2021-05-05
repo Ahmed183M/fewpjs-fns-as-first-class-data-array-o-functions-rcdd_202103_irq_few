1188f3acb6e970859abfe0d5fb41c748815cfc9f
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-05 15:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function wake(){
   console.log("Wake Byron the poodle");
 }
+function leash(){
+  console.log("Wake Byron the poodle");
+}
+function walkPark(){
+  console.log("Wake Byron the poodle");
+}
+function throws(){
+  console.log("Wake Byron the poodle");
+}
+function walkHome(){
+  console.log("Wake Byron the poodle");
+}
+function unleash(){
+  console.log("Wake Byron the poodle");
+}
 
 const functionArray = [
   wake(),
