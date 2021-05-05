ba88eff908dd8cac16a005b8712e42c228067d45
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-05 15:06:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function walkPark(dogName, dogBreed){
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 function throws(dogName, dogBreed){
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 function walkHome(dogName, dogBreed){
   console.log(`Wake ${dogName} the ${dogBreed}`);
