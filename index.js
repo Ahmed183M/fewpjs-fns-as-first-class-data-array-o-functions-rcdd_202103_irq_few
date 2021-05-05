c8080e7f8560f1a2a08e5fb572e22f6e63c6cfac
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-05 15:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function unleash(dogName, dogBreed){
 const routine = [wake(dogName, dogBreed), leash(dogName, dogBreed), walkPark(dogName, dogBreed), throws(dogName, dogBreed), walkHome(dogName, dogBreed), unleash(dogName, dogBreed)];
 
 function exerciseDog(dogName, dogBreed){
-  for(let i=0; i<routine.length){
+  for(let i=0; i<routine.length; i++){
     routine[i];
   }
 }
