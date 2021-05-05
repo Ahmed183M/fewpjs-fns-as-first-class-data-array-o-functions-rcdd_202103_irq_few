515a68a539804fb3e514f49fae7f22cc50805ea2
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-05 15:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function wake(dogName, dogBreed){
-  console.log(`Wake ${dogName} the poodle`);
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 function leash(dogName, dogBreed){
-  console.log(`Wake ${dogName} the poodle`);
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 function walkPark(dogName, dogBreed){
-  console.log(`Wake ${dogName} the poodle`);
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 function throws(dogName, dogBreed){
-  console.log(`Wake ${dogName} the poodle`);
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 function walkHome(dogName, dogBreed){
-  console.log(`Wake ${dogName} the poodle`);
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 function unleash(dogName, dogBreed){
-  console.log(`Wake ${dogName} the poodle`);
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
 const functionArray = [wake(), leash(), ]
